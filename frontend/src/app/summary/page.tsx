28c28151bb9884a783cfb6fcd78b2b70ba90b632
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-02-16 10:34:14 -0500

    added basic metadata to the page

--- frontend/src/app/summary/page.tsx
+++ frontend/src/app/summary/page.tsx
@@ -1,5 +1,12 @@
-import { Summary } from '@/components/widgets/summary_card'
 import React from 'react'
+import type { Metadata } from "next";
+
+import { Summary } from '@/components/widgets/summary_card'
+
+export const metadata: Metadata = {
+  title: "Resumen",
+  description: "Resumen de tus compras",
+}
 
 const page = () => {
   return (
