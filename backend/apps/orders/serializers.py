5a0d44fe4227c5756ce93e051fa8d813bc679731
LucasSimoesMDP <lucasgsimoes@icloud.com>
2024-02-29 18:01:03 -0300

    swagger hotfix

--- backend/apps/orders/serializers.py
+++ backend/apps/orders/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 from .models import OrderUser, ProductOrderUser
-from apps.products.models import Product
+from apps.products.serializers import ProductSerializer
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product  #correcto seria Products
-        fields = '__all__'
 
 class ProductOrderUserSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
 
     class Meta:
         model = ProductOrderUser
-        fields = ('id', 'product', 'quantity', 'comments', 'is_ready')
+        fields = ("id", "product", "quantity", "comments", "is_ready")
+
 
 class OrderUserSerializer(serializers.ModelSerializer):
     products = ProductOrderUserSerializer(many=True, read_only=True)
 
     class Meta:
         model = OrderUser
-        fields = ('id', 'table_id', 'user_id', 'products', 'is_paid', 'created_at', 'subtotal')
+        fields = (
+            "id",
+            "table_id",
+            "user_id",
+            "products",
+            "is_paid",
+            "created_at",
+            "subtotal",
+        )
