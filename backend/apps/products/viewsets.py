50512c20dff5b2232327549bb9315a3faab0aaf2
Lucas <64234755+LucasSimoesMDP@users.noreply.github.com>
2024-03-03 17:56:53 -0300

    admin auth momentarily paused for post method

--- backend/apps/products/viewsets.py
+++ backend/apps/products/viewsets.py
@@ -8,12 +8,13 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    '''
     def get_permissions(self):
         if self.action == "list":
             return [AllowAny()]
         else:
             return [IsAdminUser()]
-
+    '''
     def create(self, request, *args, **kwargs):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
@@ -30,12 +31,14 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    '''
     def get_permissions(self):
         if self.action == "list":
             return [AllowAny()]
         else:
             return [IsAdminUser()]
-
+    '''
+    
     def create(self, request, *args, **kwargs):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
